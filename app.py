d26b3db522ac04e809c05350a9abc7c1f45eb657
Vamshi45-Naredla <79595393+Vamshi45-Naredla@users.noreply.github.com>
2025-02-08 17:54:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # ✅ Add CORS
 
 app = Flask(__name__)
+CORS(app)  # ✅ Allow frontend to send requests
 
 @app.route("/")
 def home():
